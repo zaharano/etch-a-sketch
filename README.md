3cdc21737e6feedfb6113ec10d52cb2c7761daf9
Zachary Rais-Norman <zraisnorman@gmail.com>
2020-01-13 14:12:54 -0500

    refactor for modes, added scale mode, controls

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 is probably (c) somebody somewhere
 
 ##### This project is part of The Odin Project Webdev 101 curriculum.
+
+## Things and stuff
+There's something I'm not getting about `this` which is probably fine given how instructors everywhere say it's hard to fully grasp. Maybe it more has to do with... how `this` works within object declarations at runtime. Anyway I *think* that trying to do this project in some OOP style is beyond what was expected, so my failure to do it that way is probably just fine...
+
+Performance of the grayscale version at >100 or so grid size gets pretty bad. I don't know enough about DOM stuff to know if that's something that could be optimized by using some method other than setting opacity style directly on the cell. Or maybe this is something to do with async concerns, like just setting this to be async would fix the issue? Tblearned.
--- app.js
+++ app.js
@@ -1,32 +1,102 @@
-// Build the matrix of divs
-
 const gridContainer = document.getElementById('grid-container');
 
 const grid = {
-    gridSize: 64,
-    setSize(num) {
-        this.gridSize = num;
-    },
+    // default options
+    size: 64,
+    mode: 'black',
+    // builds the matrix of divs based on current size
     build() {
-        for (let i = 0; i < this.gridSize; i++) {
+        for (let i = 0; i < this.size; i++) {
             let rowDiv = document.createElement('div');
             rowDiv.classList.add('row');
             gridContainer.appendChild(rowDiv);
         }
-
         const rows = document.getElementsByClassName('row')
-
         for (let row of rows) {
-            for (let i = 0; i < this.gridSize; i++) {
-                let colDiv = document.createElement('div');
-                colDiv.classList.add('cell');
-                colDiv.addEventListener('mouseover', function() {this.classList.add('black')})
-                row.appendChild(colDiv);
+            for (let i = 0; i < this.size; i++) {
+                let cell = this.cellSetup();
+                row.appendChild(cell);
             } 
         }
+    },
+    // method to reset size with upper and lower bounds
+    // max and min on the number input don't seem to work?
+    setSize(input) {
+        if (input > 150)
+            input = 150;
+        else if (input < 2)
+            input = 2;
+        this.size = input;
+    },
+    
+    // during a build, this method is called to build each cell 
+    cellSetup() {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        return this.addCellMode(cell)
+    },
+
+    // during each cell setup, this method is called to set cell up based on mode
+    addCellMode(cell) {
+        if (this.mode === 'black') {
+            cell.addEventListener('mouseover', function() {
+                this.classList.add('black');
+            })
+            return cell;
+        } else if (this.mode === 'scale') {
+            cell.style.opacity = 0.0;
+            cell.classList.add('black');
+            cell.addEventListener('mouseover', function() {
+                this.style.opacity = Number(this.style.opacity) + 0.1;
+            })
+            return cell;
+        }
+    },
+
+    setMode(mode) {
+        this.mode = mode;
+    },
+
+    // destroys all cells and rows one by one 
+        // saw this on stackex, prevents memory leak I think?
+    // then sets the size based on input field and does a build
+    reset() {
+        const rows = document.getElementsByClassName('row')
+        for (let row of rows) {
+            while (row.firstChild) {
+                row.removeChild(row.firstChild)
+            }
+        } 
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild)
+        }
+        grid.setSize(document.querySelector('#grid-size').value);
+        grid.build();
     }
 }
 
-grid.build();
+const controls = {
+    set() {
+        const buttons = document.querySelectorAll("button");
+        for (let button of buttons) {
+            button.addEventListener("click", this[button.id])
+        }
+    },
 
+    // methods for different button click listeners 
+    hitReset() {
+        grid.reset();
+    },
+    hitBlack() {
+        grid.setMode('black');
+        grid.reset();
+    },
+    hitScale() {
+        grid.setMode('scale');
+        grid.reset();
+    },
+}
+
+grid.build();
+controls.set();
 

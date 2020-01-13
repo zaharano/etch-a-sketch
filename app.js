fa42d01cc62f33ecf40e62385f132e8960f35d0b
Zachary Rais-Norman <zraisnorman@gmail.com>
2020-01-13 10:39:45 -0500

    some style added, refactor into grid object

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 // Build the matrix of divs
-const gridSize = 200;
 
 const gridContainer = document.getElementById('grid-container');
 
-for (let i = 0; i < gridSize * .8; i++) {
-    let rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    gridContainer.appendChild(rowDiv);
+const grid = {
+    gridSize: 64,
+    setSize(num) {
+        this.gridSize = num;
+    },
+    build() {
+        for (let i = 0; i < this.gridSize; i++) {
+            let rowDiv = document.createElement('div');
+            rowDiv.classList.add('row');
+            gridContainer.appendChild(rowDiv);
+        }
+
+        const rows = document.getElementsByClassName('row')
+
+        for (let row of rows) {
+            for (let i = 0; i < this.gridSize; i++) {
+                let colDiv = document.createElement('div');
+                colDiv.classList.add('cell');
+                colDiv.addEventListener('mouseover', function() {this.classList.add('black')})
+                row.appendChild(colDiv);
+            } 
+        }
+    }
 }
 
-const rows = document.getElementsByClassName('row')
+grid.build();
 
-for (let row of rows) {
-    for (let i = 0; i < gridSize; i++) {
-        let colDiv = document.createElement('div');
-        colDiv.classList.add('cell');
-        colDiv.addEventListener('mouseover', function() {this.classList.add('drawn')})
-        row.appendChild(colDiv);
-    } 
-}
 
